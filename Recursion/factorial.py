191c91e45ba2dcde2e54dee37d409a496ede1663
Pranav Anand <pranavanand2410@gmail.com>
2021-07-10 21:56:27 +0530

    Add files via upload

---
+++ Recursion/factorial.py
@@ -0,0 +1,12 @@
+def factorial(n):
+
+    assert n>=0 and int(n) ==n, 'the number must be positive integer only!'
+
+    if n in [0,1]:
+        return 1
+    
+    else:
+        return n * factorial(n-1)
+
+print(factorial(6))
+
