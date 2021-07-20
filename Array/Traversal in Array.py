18f09cf065fc134441ef823602a2e34fb7664215
Pranav Anand <pranavanand2410@gmail.com>
2021-07-21 01:17:33 +0530

    Add files via upload

---
+++ Array/Traversal in Array.py
@@ -0,0 +1,13 @@
+from array import *
+
+arr1 = array('i', [1,2,3,4,5,6])
+
+def tarverseArray(array):
+    for i in array:
+        print(i)
+
+tarverseArray(arr1)
+
+# Time Complexity: O(n)
+
+# Space Complexity: O(1)
