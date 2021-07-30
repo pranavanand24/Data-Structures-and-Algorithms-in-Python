057e1533ce77f7877456febe91f58ff8b8eff13c
Pranav Anand <pranavanand2410@gmail.com>
2021-07-31 00:56:03 +0530

    Add files via upload

---
+++ Lists/Creating List.py
@@ -0,0 +1,11 @@
+integers = [1,2,3,4,5]
+print(integers)
+
+stringlist = ['Milk','Cheese','Chocolate','butter']
+print(stringlist)
+
+mixList = [1,2,3,4,5.5, 'spam']
+print(mixList)
+
+nestedList = [1,2,3,4, [1.4,1.6,1.7], ['test']]
+print(nestedList)
