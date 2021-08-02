842f68dca091128cf74fc6fa211854e25dca50ff
Pranav Anand <pranavanand2410@gmail.com>
2021-08-03 01:00:56 +0530

    Add files via upload

---
+++ Lists/Accessing and Traversing a list.py
@@ -0,0 +1,15 @@
+shoppinglist = ['Milk','Cheese','butter']
+
+print(shoppinglist)
+
+# in operator
+
+print('Milk' in shoppinglist)
+
+# loop 
+
+for i in range(len(shoppinglist)):
+    shoppinglist[i] = shoppinglist[i]
+    print(shoppinglist[i])
+
+
