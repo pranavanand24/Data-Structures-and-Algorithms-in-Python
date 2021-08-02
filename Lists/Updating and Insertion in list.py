faa31681abb147e49545ed3ce3ec4034937a036a
Pranav Anand <pranavanand2410@gmail.com>
2021-08-03 01:01:09 +0530

    Add files via upload

---
+++ Lists/Updating and Insertion in list.py
@@ -0,0 +1,25 @@
+myList = [1,2,3,4,5,6,7,8,9,10]
+
+# update elements from list
+
+myList[2] = 33
+
+print(myList)
+
+# insert method
+
+myList.insert(0,11)
+
+print(myList)
+
+# append method
+
+myList.append(55)
+
+print(myList)
+
+# extend method
+
+newlist = [14,15,16,17,18]
+
+print(myList)
