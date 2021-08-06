35cba4c16b8ea4da2fbf5d3ab3cfb35d19e1635e
Pranav Anand <pranavanand2410@gmail.com>
2021-08-07 00:47:26 +0530

    Add files via upload

---
+++ Tuple/Creating a tuple.py
@@ -0,0 +1,14 @@
+newTuple = ('a','b','c','d','e','f')
+
+newTuple2 = ('a',)
+
+newTuple1 = tuple('d')
+
+print(newTuple)
+print(newTuple1)
+print(newTuple2)
+
+# Time Complexity - O(1)
+# Space Complexity - O(n)
+
+
