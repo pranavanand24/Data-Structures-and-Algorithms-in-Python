c318a195f1f8e33390940a2900167309f83f91f3
Pranav Anand <pranavanand2410@gmail.com>
2021-08-12 20:31:19 +0530

    Add files via upload

---
+++ Linked List/Seaching in single LinkedList.py
@@ -0,0 +1,62 @@
+class Node:
+    def __init__(self, value=None):
+        self.value = value
+        self.next = None
+
+class SLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+    def __iter__(self):
+        node = self.head
+        while node:
+            yield node
+            node = node.next
+
+    # insert in Linked List
+    
+    def insertSLL(self, value, location):
+        newNode = Node(value)
+        if self.head is None:
+            self.head = newNode
+            self.tail = newNode
+        else:
+            if location == 0:
+                newNode.next = self.head
+                self.head = newNode
+            elif location == 1:
+                newNode.next = None
+                self.tail.next = newNode
+                self.tail = newNode
+            else:
+                tempNode = self.head
+                index = 0
+                while index < location - 1:
+                    tempNode = tempNode.next
+                    index += 1
+                nextNode = tempNode.next
+                tempNode.next = newNode
+                newNode.next = nextNode
+
+    def SearchSLL(self,nodeValue):
+        if self.head is None:
+            return "the list does not exist"
+        else:
+            node = self.head
+            while node is not None:
+                if node.value == nodeValue:
+                    return node.next
+            return "the value does not exist in this list"
+
+singlyLinkedList = SLinkedList()
+singlyLinkedList.insertSLL(1, 1)
+singlyLinkedList.insertSLL(2, 1)
+singlyLinkedList.insertSLL(3, 1)
+singlyLinkedList.insertSLL(4, 1)
+
+
+print([node.value for node in singlyLinkedList])
+print(singlyLinkedList.SearchSLL(3))
+
+# Time Complexity - O(n)
+# Space Complexity - O(1)
