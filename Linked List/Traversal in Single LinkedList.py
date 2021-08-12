b4874f90467731ca9ff42b18b0cd3e134f3c1cc3
Pranav Anand <pranavanand2410@gmail.com>
2021-08-12 13:22:35 +0530

    Add files via upload

---
+++ Linked List/Traversal in Single LinkedList.py
@@ -0,0 +1,61 @@
+class Node:
+    def __init__(self, value=None):
+        self.value = value
+        self.next = None
+
+class SLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+    def __iter__(self):
+        node = self.head
+        while node:
+            yield node
+            node = node.next
+
+    # insert in Linked List
+    
+    def insertSLL(self, value, location):
+        newNode = Node(value)
+        if self.head is None:
+            self.head = newNode
+            self.tail = newNode
+        else:
+            if location == 0:
+                newNode.next = self.head
+                self.head = newNode
+            elif location == 1:
+                newNode.next = None
+                self.tail.next = newNode
+                self.tail = newNode
+            else:
+                tempNode = self.head
+                index = 0
+                while index < location - 1:
+                    tempNode = tempNode.next
+                    index += 1
+                nextNode = tempNode.next
+                tempNode.next = newNode
+                newNode.next = nextNode
+
+    def traverseSLL(self):
+        if self.head is None:
+            print("the single linkedList doesn't exist")
+
+        else:
+            node = self.head
+            while node is not None:
+                print(node.value)
+                node = node.next
+
+singlyLinkedList = SLinkedList()
+singlyLinkedList.insertSLL(1, 1)
+singlyLinkedList.insertSLL(2, 1)
+singlyLinkedList.insertSLL(3, 1)
+singlyLinkedList.insertSLL(4, 1)
+
+print([node.value for node in singlyLinkedList])
+singlyLinkedList.traverseSLL()
+
+# Time Complexity - O(n)
+# Space Complexity - O(1)
