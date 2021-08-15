370024d3b024ffc12d67274bcce50122747f41d3
Pranav Anand <pranavanand2410@gmail.com>
2021-08-16 02:06:55 +0530

    Add files via upload

---
+++ Linked List/Deleting Entire SinglyLinkedList.py
@@ -0,0 +1,68 @@
+class Node:
+    def __init__(self, value=None):
+        self.value = value
+        self.next = None
+
+class SLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+    def __iter__(self):
+        node = self.head
+        while node:
+            yield node
+            node = node.next
+    # insert in Linked List
+    def insertSLL(self, value, location):
+        newNode = Node(value)
+        if self.head is None:
+            self.head = newNode
+            self.tail = newNode
+        else:
+            if location == 0:
+                newNode.next = self.head
+                self.head = newNode
+            elif location == 1:
+                newNode.next = None
+                self.tail.next = newNode
+                self.tail = newNode
+            else:
+                tempNode = self.head
+                index = 0
+                while index < location - 1:
+                    tempNode = tempNode.next
+                    index += 1
+                nextNode = tempNode.next
+                tempNode.next = newNode
+                newNode.next = nextNode
+
+    # Delete entire SLL
+    def deleteEntireSLL(self):
+        if self.head is None:
+            print("The SLL does not exist")
+        else:
+            self.head = None
+            self.tail = None
+
+
+singlyLinkedList = SLinkedList()
+singlyLinkedList.insertSLL(1, 1)
+singlyLinkedList.insertSLL(2, 1)
+singlyLinkedList.insertSLL(3, 1)
+singlyLinkedList.insertSLL(4, 1)
+singlyLinkedList.insertSLL(0, 0)
+singlyLinkedList.insertSLL(0, 4)
+
+
+print([node.value for node in singlyLinkedList]) 
+singlyLinkedList.deleteEntireSLL()
+print([node.value for node in singlyLinkedList])
+
+# Time Complexity - O(1)
+# Space Complexity - O(1)
+
+
+
+
+
+
