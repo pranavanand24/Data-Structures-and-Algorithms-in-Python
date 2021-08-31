b41f37038b45a0d0afca7c8817556b90f5a3026b
Pranav Anand <pranavanand2410@gmail.com>
2021-08-31 18:13:50 +0530

    Add files via upload

---
+++ Stack/Create Stack Using List.py
@@ -0,0 +1,11 @@
+class stack:
+    def __init__(self):
+        self.list = []
+
+    def __str__(self):
+        values = self.list.reverse()
+        values = [str(x) for x in self.list]
+        return '\n'.join(values)
+
+
+
