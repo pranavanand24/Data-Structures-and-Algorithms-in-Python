74dc27c89e3b4f6545780174d4a0ca80a86e2a58
Pranav Anand <pranavanand2410@gmail.com>
2021-09-04 00:18:01 +0530

    Add files via upload

---
+++ Stack/Create Stack with limit.py
@@ -0,0 +1,63 @@
+class Stack:
+    def __init__(self,maxSize):
+        self.maxSize = maxSize
+        self.list = []
+        
+    def __str__(self):
+        values = self.list.reverse()
+        values = [str(x) for x in self.list]
+        return '\n'.join(values)
+        
+    # isEmpty
+    
+    def isEmpty(self):
+        if self.list == []:
+            return True
+        else:
+            return False
+    
+    # isFull
+    
+    def isFull(self):
+        if len(self.list) == self.maxSize:
+            return True
+        else:
+            return False
+            
+    # push
+    
+    def push(self,value):
+        if self.isFull():
+            return "the stack is full"
+        else:
+            self.list.append(value)
+            return "the element has been successfully inserted"
+            
+    # pop
+    
+    def pop(self):
+        if self.isEmpty():
+            return "there is not any element in the stack"
+        else:
+            return self.list.pop()
+            
+    # peek
+    
+    def pop(self):
+        if self.isEmpty():
+            return "there is not any element in the stack"
+        else:
+            return self.list[len(self.list)-1]
+            
+    # delete
+    
+    def delete(self):
+        self.list = None
+            
+customStack = Stack(4)
+print(customStack.isEmpty())
+print(customStack.isFull())
+customStack.push(1)
+customStack.push(2)
+customStack.push(3)
+print(customStack)
